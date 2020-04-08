aaceda27c39b0aff9172fe86763faa12b1d9d5ca
Quino <quino.mandersisc@nl.ibm.com>
2020-04-08 14:15:20 +0200

    third commit

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -9,13 +10,16 @@ export class FooterComponent implements OnInit {
   public counter: boolean;
   public results: boolean;
   public stores: boolean;
-  public invertedView: boolean;
-  public constructor() { }
+
+  public constructor(private router: Router) { }
 
   public ngOnInit(): void {
     this.counter = false;
     this.results = false;
     this.stores = false;
+
+    console.log('location', window.location.href);
+    console.log('location 2', this.router.url);
 
     if ((window.location.href).includes('results')) {
       this.results = true;
